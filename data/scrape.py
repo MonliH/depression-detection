eb1a8bb5433cacf54ac7139edd9da6ff9bb31742
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2023-02-18 13:29:46 -0500

    Use normal api

--- data/scrape.py
+++ data/scrape.py
@@ -1,16 +1,30 @@
-import jsonlines
-from pmaw import PushshiftAPI
+import json
+import requests
 
-api = PushshiftAPI()
-gen = api.search_comments(q="diagnosed depression", limit=1000000)
+past_date = 1676730916
 
-with jsonlines.open('output/comments.jsonl', mode='w') as writer:
-    for comment in gen:
-        value = {}
-        value["text"] = comment["body"]
-        value["author"] = comment["author"]
-        value["created_utc"] = comment["created_utc"]
-        value["subreddit"] = comment["subreddit"]
-        value["score"] = comment["score"]
-        value["id"] = comment["id"]
-        writer.write(value)
+running = []
+n = 0
+while True:
+    res = requests.get("https://api.pushshift.io/reddit/comment/search", dict(
+        q="diagnosed depression", filter=",".join(["body", "author", "created_utc", "subreddit", "score", "id"]),
+        limit=1000, until=past_date
+    )).json()
+    running.extend(res["data"])
+    last = running[-1]["created_utc"]
+
+    assert last <= past_date
+    past_date = last
+
+    if len(running) >= 50000:
+        past_n = n
+        n += len(running)
+        with open(f'output/comments_{past_n}_{n}.jsonl', mode='w') as writer:
+            for entry in running:
+                json.dump(entry, writer)
+                writer.write("\n")
+        
+        running.clear()
+    
+    if n > 10000000:
+        break
